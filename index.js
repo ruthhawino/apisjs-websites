0da39c110ada4fe99c5bce65034184414e7fcc39
ruthhawino <awinoruthh@gmail.com>
2023-06-04 18:02:20 +0300

    styledwith javascript

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+//  https://dummyjson.com/products
+function generateProductCards(data) {
+    const products = document.getElementById("products");
+    // iterate through the data and create a card for each product
+    data.forEach((product) => {
+      // create a card element
+      const card = document.createElement("div");
+      card.classList.add("product-card");
+      // Set the card content using the product data
+      card.innerHTML = `
+              <img src="${product.thumbnail}" alt="${product.name}" />
+              <h2>${product.title}</h2>
+              <div class='productPrice'>
+              <p>${product.price}</p>
+              <button>ADD TO CART</button>
+              </div>
+          `;
+      // append the card to the product container
+      products.appendChild(card);
+    });
+  }
+  function fetchProducts() {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        // Get the first 8 products from the data
+        const returnedData = data.products.slice(0, 8);
+        // call the function to generate cards
+        generateProductCards(returnedData);
+      });
+  }
+  fetchProducts()
